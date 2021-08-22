e54e2e5da75c39ea71ea03cc8db7e3bfa7073a99
ItsWagPvP <itswagpvp@gmail.com>
2021-08-22 18:42:35 +0200

    MySQL autoReconnect and useSSL option

--- src/main/java/me/itswagpvp/economyplus/storage/mysql/MySQL.java
+++ src/main/java/me/itswagpvp/economyplus/storage/mysql/MySQL.java
@@ -21,8 +21,9 @@ public class MySQL {
     final String port = plugin.getConfig().getString("Database.Port");
     final String database = plugin.getConfig().getString("Database.Database");
     final String table = plugin.getConfig().getString("Database.Table");
+    final boolean autoReconnect = plugin.getConfig().getBoolean("Database.AutoReconnect");
 
-    final String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+    final String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?autoReconnect=" + autoReconnect + "&useSSL=false";
 
     static Connection connection;
 
