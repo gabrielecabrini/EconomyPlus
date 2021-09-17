75f5f9a0588b558a283a1714e67f98da0489fd38
DevWhiteFox <76470694+DevWhiteFox@users.noreply.github.com>
2021-09-17 15:50:01 +0200

    Refactor move "DatabaseType", add functions, static "dbType" gettable with "getDBType" and refactor "EconomyPlus" adapting it for "DatabaseType" tasks

--- src/main/java/me/itswagpvp/economyplus/misc/DatabaseType.java
+++ src/main/java/me/itswagpvp/economyplus/misc/DatabaseType.java
@@ -1,9 +1,14 @@
-package me.itswagpvp.economyplus.vault;
+package me.itswagpvp.economyplus.misc;
 
 import me.itswagpvp.economyplus.EconomyPlus;
 import me.itswagpvp.economyplus.dbStorage.mysql.MySQL;
+import org.apache.commons.lang.NotImplementedException;
 
-enum DatabaseType {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum DatabaseType {
     H2{
         @Override
         public boolean contains(String playerName) {
@@ -28,6 +33,16 @@ enum DatabaseType {
         @Override
         public double getBank(String playerName) {
             return EconomyPlus.getInstance().getRDatabase().getBank(playerName);
+        }
+
+        @Override
+        public List<String> getList() {
+            return EconomyPlus.getInstance().getRDatabase().getList();
+        }
+
+        @Override
+        public void close() throws SQLException {
+            EconomyPlus.getInstance().getRDatabase().getSQLiteConnection().close();
         }
     },
     MySQL{
@@ -55,6 +70,88 @@ enum DatabaseType {
         public double getBank(String playerName) {
             return new MySQL().getBank(playerName);
         }
+
+        @Override
+        public List<String> getList() {
+            return new MySQL().getList();
+        }
+
+        @Override
+        public void close() {
+            new MySQL().closeConnection();
+        }
+    },
+    Json{
+        @Override
+        public boolean contains(String playerName) {
+            return false;
+        }
+
+        @Override
+        public double getToken(String playerName) {
+            return 0;
+        }
+
+        @Override
+        public void setTokens(String playerName, double tokens) {
+
+        }
+
+        @Override
+        public void setBank(String playerName, double tokens) {
+
+        }
+
+        @Override
+        public double getBank(String playerName) {
+            return 0;
+        }
+
+        @Override
+        public List<String> getList() {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public void close() {
+            throw new NotImplementedException();
+        }
+    },
+    Yaml{
+        @Override
+        public boolean contains(String playerName) {
+            return false;
+        }
+
+        @Override
+        public double getToken(String playerName) {
+            return 0;
+        }
+
+        @Override
+        public void setTokens(String playerName, double tokens) {
+
+        }
+
+        @Override
+        public void setBank(String playerName, double tokens) {
+
+        }
+
+        @Override
+        public double getBank(String playerName) {
+            return 0;
+        }
+
+        @Override
+        public List<String> getList() {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public void close() {
+            throw new NotImplementedException();
+        }
     },
     Undefined{
         @Override
@@ -77,6 +174,14 @@ enum DatabaseType {
         public double getBank(String playerName) {
             return 0D;
         }
+
+        @Override
+        public List<String> getList() {
+            return new ArrayList<>();
+        }
+
+        @Override
+        public void close() {}
     };
 
     public abstract boolean contains(String playerName);
@@ -84,5 +189,6 @@ enum DatabaseType {
     public abstract void setTokens(String playerName, double tokens);
     public abstract void setBank(String playerName, double tokens);
     public abstract double getBank(String playerName);
-
+    public abstract List<String> getList();
+    public abstract void close() throws SQLException;
 }
