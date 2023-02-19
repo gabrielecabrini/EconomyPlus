a7aae8e561cf823542fa0be8c55897d0e9487eaf
Jake Pronger <jakepronger31@gmail.com>
2023-02-19 12:15:18 +1100

    reformatted economy command

--- src/main/java/me/itswagpvp/economyplus/commands/Eco.java
+++ src/main/java/me/itswagpvp/economyplus/commands/Eco.java
@@ -11,11 +11,6 @@ import org.bukkit.command.CommandSender;
 import me.itswagpvp.economyplus.misc.Utils;
 import me.itswagpvp.economyplus.vault.Economy;
 import org.bukkit.entity.Player;
-import org.bukkit.util.StringUtil;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static me.itswagpvp.economyplus.EconomyPlus.plugin;
 
@@ -24,45 +19,102 @@ public class Eco implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        OfflinePlayer p = null;
+        if (!Utils.hasPerm(sender, "economyplus.eco.reset") || !Utils.hasPerm(sender, "economyplus.eco.give") || !Utils.hasPerm(sender, "economyplus.eco.take") || !Utils.hasPerm(sender, "economyplus.eco.set")) {
+            return true;
+        }
 
-        boolean all = false;
+        if (args.length == 2) {
 
-        if (args.length >= 1) {
+            OfflinePlayer p;
 
             String name = args[0];
 
-            if ((name.equalsIgnoreCase("@a") || name.equalsIgnoreCase("*") && args.length == 2 && args[1].equalsIgnoreCase("reset"))) {
-                all = true;
-            }
+            double starting = plugin.getConfig().getDouble("Starting-Balance"); // gets starting balance
 
-            if (!all) { // all flag wasn't used and args[0] is a player's username
-                p = Bukkit.getOfflinePlayer(args[0]);
-            }
+            if (args[1].equalsIgnoreCase("reset")) {
 
-        }
+                if (name.equalsIgnoreCase("@a") || name.equalsIgnoreCase("*")) {
 
-        if (!all) {
+                    // is using @a or *
 
-            if (p == null) {
-                sender.sendMessage(plugin.getMessage("PlayerNotFound"));
-                return false;
-            }
+                    if (plugin.getConfig().getBoolean("Reset-All.Flags", false)) {
 
-            if (plugin.getConfig().getBoolean("Invalid-Users.Modify-Balance", false)) {
-                PlayerHandler.saveName(p);
-            } else { // Modifying invalid users is disabled in config.
-                if (!p.hasPlayedBefore() && !p.isOnline()) {
-                    // player hasn't joined before and isn't currently online
-                    sender.sendMessage(ChatColor.RED + args[0] + " hasn't joined before.");
-                    Utils.playErrorSound(sender);
-                    return false;
+                        if (plugin.getConfig().getBoolean("Reset-All.Console-Only", true)) {
+                            if (sender instanceof Player) {
+                                // player is not console
+                                sender.sendMessage(ChatColor.RED + "Only console can use @a and * flags!");
+                                return true;
+                            }
+                        }
+
+                    } else {
+                        sender.sendMessage(ChatColor.RED + "Resetting everyone's balance is disabled!");
+                        Utils.playErrorSound(sender);
+                        return true;
+                    }
+
+                    for (OfflinePlayer op : Bukkit.getOfflinePlayers()) {
+
+                        Economy eco = new Economy(op);
+                        eco.setBalance(starting);
+
+                        if (op.getPlayer() != null && op.isOnline()) {
+                            op.getPlayer().sendMessage(plugin.getMessage("Money.Reset"));
+                            Utils.playErrorSound(op.getPlayer());
+                        }
+
+                    }
+
+                    if (plugin.isMessageEnabled("Money.Done")) {
+                        sender.sendMessage(ChatColor.GREEN + "You have just refreshed everyone's balances!");
+                    }
+
+                    Utils.playSuccessSound(sender);
+
+                } else {
+
+                    // isn't using * or @a
+
+                    p = Bukkit.getOfflinePlayer(args[0]);
+
+                    if (!(p.hasPlayedBefore() || p.isOnline())) {
+
+                        if (plugin.SET_INVALID) {
+                            PlayerHandler.saveName(p);
+                        } else {
+                            // player hasn't joined before and isn't currently online
+                            sender.sendMessage(ChatColor.RED + args[0] + " hasn't joined before.");
+                            Utils.playErrorSound(sender);
+                            return true;
+                        }
+
+                    }
+
+                    if (!Utils.hasPerm(sender, "economyplus.eco.reset")) {
+                        return true;
+                    } else {
+
+                        Economy eco = new Economy(p);
+                        eco.setBalance(starting);
+
+                        if (plugin.isMessageEnabled("Money.Done")) {
+                            sender.sendMessage(plugin.getMessage("Money.Done"));
+                        }
+
+                        if (p.isOnline() && p.getPlayer() != null) {
+                            p.getPlayer().sendMessage(plugin.getMessage("Money.Reset"));
+                        }
+
+                        Utils.playSuccessSound(sender);
+
+                    }
+
                 }
+
+                return true;
             }
 
-        }
-
-        if (args.length == 3) {
+        } else if (args.length == 3) {
 
             if (!(args[1].equalsIgnoreCase("set") || args[1].equalsIgnoreCase("give") || args[1].equalsIgnoreCase("take"))) {
                 sender.sendMessage(plugin.getMessage("InvalidArgs.Eco"));
@@ -79,6 +131,8 @@ public class Eco implements CommandExecutor {
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cInvalid number!"));
                 return true;
             }
+
+            OfflinePlayer p = Bukkit.getOfflinePlayer(args[0]);
 
             Economy money = new Economy(p);
             Utils utility = new Utils();
@@ -140,7 +194,9 @@ public class Eco implements CommandExecutor {
                 Utils.playSuccessSound(sender);
 
                 return true;
+
             } else if (args[1].equalsIgnoreCase("give")) {
+
                 if (!Utils.hasPerm(sender, "economyplus.eco.give")) {
                     return true;
                 }
@@ -161,81 +217,12 @@ public class Eco implements CommandExecutor {
                 }
 
                 Utils.playSuccessSound(sender);
-
                 return true;
-            }
-
-            sender.sendMessage(plugin.getMessage("InvalidArgs.Eco"));
-            Utils.playErrorSound(sender);
-
-            return true;
-
-        } else if (args.length == 2) {
-
-            if (args[1].equalsIgnoreCase("reset")) {
-
-                if (!Utils.hasPerm(sender, "economyplus.eco.reset")) {
-                    return true;
-                }
-
-                double starting = plugin.getConfig().getDouble("Starting-Balance"); // gets starting balance
-
-                if (all) { // wild flag used (@a or *)
-
-                    if (!(plugin.getConfig().getBoolean("Invalid-Users.Allow-Reset-All"))) {
-                        sender.sendMessage(ChatColor.RED + "Resetting everyone's balance is disabled in config!");
-                        Utils.playErrorSound(sender);
-                        return true;
-                    }
-
-                    for (OfflinePlayer op : Bukkit.getOfflinePlayers()) {
-
-                        Economy eco = new Economy(op);
-                        eco.setBalance(starting);
-
-                        if (op.getPlayer() != null && op.isOnline()) {
-                            op.getPlayer().sendMessage(plugin.getMessage("Money.Reset"));
-                            Utils.playErrorSound(op.getPlayer());
-                        }
-
-                    }
-
-                    if (plugin.isMessageEnabled("Money.Done")) {
-                        sender.sendMessage(ChatColor.GREEN + "You have just refreshed everyone's balances!");
-                    }
-
-                    Utils.playSuccessSound(sender);
-
-                }
-
-                else {
-
-                    Economy eco = new Economy(p);
-                    eco.setBalance(starting);
-
-                    if (plugin.isMessageEnabled("Money.Done")) {
-                        sender.sendMessage(plugin.getMessage("Money.Done"));
-                    }
-
-                    if (p.getPlayer() != null) {
-                        p.getPlayer().sendMessage(plugin.getMessage("Money.Reset"));
-                        Utils.playErrorSound(p.getPlayer());
-                    }
-
-                    Utils.playSuccessSound(sender);
-
-                }
-
-                return true;
-
             }
 
         }
 
-        if (!Utils.hasPerm(sender, "economyplus.eco.reset") || !Utils.hasPerm(sender, "economyplus.eco.give") || !Utils.hasPerm(sender, "economyplus.eco.take") || !Utils.hasPerm(sender, "economyplus.eco.set")) {
-            return true;
-        }
-
+        // usage
         sender.sendMessage(plugin.getMessage("InvalidArgs.Eco"));
         Utils.playErrorSound(sender);
         return true;
