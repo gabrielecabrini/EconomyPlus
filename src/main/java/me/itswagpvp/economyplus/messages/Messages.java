061f2e19c790d5e6cb2c7a01b8434b4f52523394
Jake Pronger <jakepronger31@gmail.com>
2023-02-19 11:16:49 +1100

    new method for how messages load their files. (No longer need to have a list containing the names of the language files)

--- src/main/java/me/itswagpvp/economyplus/messages/Messages.java
+++ src/main/java/me/itswagpvp/economyplus/messages/Messages.java
@@ -1,17 +1,20 @@
 package me.itswagpvp.economyplus.messages;
 
-import me.itswagpvp.economyplus.EconomyPlus;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import static me.itswagpvp.economyplus.EconomyPlus.plugin;
 
@@ -19,19 +22,13 @@ public class Messages {
 
     private static final String path = plugin.getDataFolder() + "/messages";
 
-    //Could change to loop through recourses folder but decided not to because of other files that would conflict.
-    private static final List<String> list = Arrays.asList("EN", "IT", "RO", "AL", "DE", "ZH", "FR", "RU", "ES"); //language file names
-
     public static void load() {
 
-        // loop through all files in recourses folder if it has 2 chars and capitalize treat it as a language file
+        List<String> languages = getLanguages();
 
-        //File dir = new File();
-        //File[] directoryListing = dir.listFiles();
+        for (String name : languages) {
 
-        for (String name : list) {
-
-            File file = new File(path, name + ".yml");
+            File file = new File(path, name);
 
             if (!file.exists()) {
 
@@ -66,6 +63,44 @@ public class Messages {
             return YamlConfiguration.loadConfiguration(new File(path, name + ".yml"));
         }
         return null;
+    }
+
+    public static List<String> getLanguages() { // returns a list of file names that don't contain a / in the path
+
+        // loop through all files in recourses folder
+        // if it has 2 chars as the name (excluding .yml)
+        // treat it as a language file
+
+        List<String> list = new ArrayList<>();
+
+        try {
+
+            String s = new File(plugin.getClass().getProtectionDomain().getCodeSource().getLocation()
+                    .toURI()).getPath();
+
+            JarFile jarFile = new JarFile(s);
+            Enumeration<? extends JarEntry> entries = jarFile.entries();
+
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String name = entry.getName();
+                if (!name.contains("/") && name.contains(".yml") && name.length() == 6) {
+                    list.add(name);
+                }
+            }
+
+            return list;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[Economy Plus] Error loading languages!");
+        } catch (URISyntaxException e) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "[Economy Plus] Error loading languages!");
+            throw new RuntimeException(e);
+        }
+
+        return list;
+
     }
 
 }
